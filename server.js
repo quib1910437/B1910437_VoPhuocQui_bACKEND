4ca518c1860ac7fb7a72daeb32ebeed2b1a543dd
quib1910437 <122423268+quib1910437@users.noreply.github.com>
2023-02-22 15:06:58 +0700

    up

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const config = require("./app/config");
-
-
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Sever is running on port ${PORT}.`);
-});
