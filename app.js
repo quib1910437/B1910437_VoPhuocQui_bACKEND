a8dc79dae199d9abf35a4c99ef0981e925754451
quib1910437 <122423268+quib1910437@users.noreply.github.com>
2023-02-25 07:23:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const contactsRouter = require("./app/routes/contact.route");
 
-const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
+
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+
+app.use("/api/contacts",contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
-app.use("/api/contacts",contactsRouter);
-
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/untils/mongodb.util");
 
+async function startSever(){
+    try{
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+        console.log(`Sever is running on port ${PORT}`);
+    });
+    } catch (error){
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Sever is running on port ${PORT}.`);
-});
+startSever();
